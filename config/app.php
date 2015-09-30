d9b5c09c786cceec5dee601c9a57b22767b0aab7
Gynidark <gynidark@hotmail.com>
2015-09-30 18:19:46 +0200

    Ajout app.php

---
+++ config/app.php
@@ -0,0 +1,259 @@
+<?php
+return [
+
+    'debug' => true,
+
+    'App' => [
+        'namespace' => 'App',
+        'encoding' => 'UTF-8',
+        'base' => false,
+        'dir' => 'src',
+        'webroot' => 'webroot',
+        'wwwRoot' => WWW_ROOT,
+        // 'baseUrl' => env('SCRIPT_NAME'),
+        'fullBaseUrl' => false,
+        'imageBaseUrl' => 'img/',
+        'cssBaseUrl' => 'css/',
+        'jsBaseUrl' => 'js/',
+        'paths' => [
+            'plugins' => [ROOT . DS . 'plugins' . DS],
+            'templates' => [APP . 'Template' . DS],
+            'locales' => [APP . 'Locale' . DS],
+        ],
+    ],
+
+    'Security' => [
+        'salt' => '1b73254909056ff453c44dc690599e26980e9de9afa8cb7eed3a9a2b36bfe607',
+    ],
+
+    'Asset' => [
+        //'timestamp' => true,
+    ],
+
+    /**
+     * Configure the cache adapters.
+     */
+    'Cache' => [
+        'default' => [
+            'className' => 'File',
+            'path' => CACHE,
+        ],
+
+        'menu' => [
+            'className' => 'File',
+            'prefix' => 'OranTicket_menu_',
+            'path' => CACHE . 'menu/',
+            'duration' => '+1 days',
+        ],
+
+        'footer' => [
+            'className' => 'File',
+            'prefix' => 'OranTicket_footer_',
+            'path' => CACHE . 'footer/',
+            'duration' => '+1 days',
+        ],
+
+        'paginate' => [
+            'className' => 'File',
+            'prefix' => 'OranTicket_paginate_',
+            'path' => CACHE . 'paginate/',
+            'duration' => '+1 days',
+        ],
+
+        /**
+         * Configure the cache used for general framework caching. Path information,
+         * object listings, and translation cache files are stored with this
+         * configuration.
+         */
+        '_cake_core_' => [
+            'className' => 'File',
+            'prefix' => 'myapp_cake_core_',
+            'path' => CACHE . 'persistent/',
+            'serialize' => true,
+            'duration' => '+2 minutes',
+        ],
+
+        /**
+         * Configure the cache for model and datasource caches. This cache
+         * configuration is used to store schema descriptions, and table listings
+         * in connections.
+         */
+        '_cake_model_' => [
+            'className' => 'File',
+            'prefix' => 'myapp_cake_model_',
+            'path' => CACHE . 'models/',
+            'serialize' => true,
+            'duration' => '+2 minutes',
+        ],
+    ],
+
+    /**
+     * Configure the Error and Exception handlers used by your application.
+     *
+     * By default errors are displayed using Debugger, when debug is true and logged
+     * by Cake\Log\Log when debug is false.
+     *
+     * In CLI environments exceptions will be printed to stderr with a backtrace.
+     * In web environments an HTML page will be displayed for the exception.
+     * With debug true, framework errors like Missing Controller will be displayed.
+     * When debug is false, framework errors will be coerced into generic HTTP errors.
+     *
+     * Options:
+     *
+     * - `errorLevel` - int - The level of errors you are interested in capturing.
+     * - `trace` - boolean - Whether or not backtraces should be included in
+     *   logged errors/exceptions.
+     * - `log` - boolean - Whether or not you want exceptions logged.
+     * - `exceptionRenderer` - string - The class responsible for rendering
+     *   uncaught exceptions.  If you choose a custom class you should place
+     *   the file for that class in src/Error. This class needs to implement a
+     *   render method.
+     * - `skipLog` - array - List of exceptions to skip for logging. Exceptions that
+     *   extend one of the listed exceptions will also be skipped for logging.
+     *   E.g.:
+     *   `'skipLog' => ['Cake\Network\Exception\NotFoundException', 'Cake\Network\Exception\UnauthorizedException']`
+     */
+    'Error' => [
+        'errorLevel' => E_ALL & ~E_DEPRECATED,
+        'exceptionRenderer' => 'Cake\Error\ExceptionRenderer',
+        'skipLog' => [],
+        'log' => true,
+        'trace' => true,
+    ],
+
+    'EmailTransport' => [
+        'default' => [
+            'className' => 'Smtp',
+            'host' => '127.0.0.1',
+            'port' => 1025,
+            'timeout' => 30,
+            'username' => null,
+            'password' => null,
+            'client' => null,
+            'tls' => null,
+        ],
+    ],
+
+    'Email' => [
+        'default' => [
+            'transport' => 'default',
+            'from' => 'support@oranticket.fr',
+            'charset' => 'utf-8',
+            'headerCharset' => 'utf-8',
+        ],
+    ],
+
+    'Datasources' => [
+        'default' => [
+            'className' => 'Cake\Database\Connection',
+            'driver' => 'Cake\Database\Driver\Mysql',
+            'persistent' => false,
+            'host' => 'localhost',
+            //'port' => 'nonstandard_port_number',
+            'username' => 'root',
+            'password' => 'root',
+            'database' => 'oranticket',
+            'encoding' => 'utf8',
+            'timezone' => 'UTC',
+            'cacheMetadata' => true,
+
+            /**
+             * Set identifier quoting to true if you are using reserved words or
+             * special characters in your table or column names. Enabling this
+             * setting will result in queries built using the Query Builder having
+             * identifiers quoted when creating SQL. It should be noted that this
+             * decreases performance because each query needs to be traversed and
+             * manipulated before being executed.
+             */
+            'quoteIdentifiers' => false,
+
+            /**
+             * During development, if using MySQL < 5.6, uncommenting the
+             * following line could boost the speed at which schema metadata is
+             * fetched from the database. It can also be set directly with the
+             * mysql configuration directive 'innodb_stats_on_metadata = 0'
+             * which is the recommended value in production environments
+             */
+            //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
+        ],
+
+        /**
+         * The test connection is used during the test suite.
+         */
+        'test' => [
+            'className' => 'Cake\Database\Connection',
+            'driver' => 'Cake\Database\Driver\Mysql',
+            'persistent' => false,
+            'host' => 'localhost',
+            //'port' => 'nonstandard_port_number',
+            'username' => 'my_app',
+            'password' => 'secret',
+            'database' => 'test_myapp',
+            'encoding' => 'utf8',
+            'timezone' => 'UTC',
+            'cacheMetadata' => true,
+            'quoteIdentifiers' => false,
+            //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
+        ],
+    ],
+
+    /**
+     * Configures logging options
+     */
+    'Log' => [
+        'debug' => [
+            'className' => 'Cake\Log\Engine\FileLog',
+            'path' => LOGS,
+            'file' => 'debug',
+            'levels' => ['notice', 'info', 'debug'],
+        ],
+        'error' => [
+            'className' => 'Cake\Log\Engine\FileLog',
+            'path' => LOGS,
+            'file' => 'error',
+            'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
+        ],
+    ],
+
+    /**
+     * Session configuration.
+     *
+     * Contains an array of settings to use for session configuration. The
+     * `defaults` key is used to define a default preset to use for sessions, any
+     * settings declared here will override the settings of the default config.
+     *
+     * ## Options
+     *
+     * - `cookie` - The name of the cookie to use. Defaults to 'CAKEPHP'.
+     * - `cookiePath` - The url path for which session cookie is set. Maps to the
+     *   `session.cookie_path` php.ini config. Defaults to base path of app.
+     * - `timeout` - The time in minutes the session should be valid for.
+     *    Pass 0 to disable checking timeout.
+     *    Please note that php.ini's session.gc_maxlifetime must be equal to or greater
+     *    than the largest Session['timeout'] in all served websites for it to have the
+     *    desired effect.
+     * - `defaults` - The default configuration set to use as a basis for your session.
+     *    There are four built-in options: php, cake, cache, database.
+     * - `handler` - Can be used to enable a custom session handler. Expects an
+     *    array with at least the `engine` key, being the name of the Session engine
+     *    class to use for managing the session. CakePHP bundles the `CacheSession`
+     *    and `DatabaseSession` engines.
+     * - `ini` - An associative array of additional ini values to set.
+     *
+     * The built-in `defaults` options are:
+     *
+     * - 'php' - Uses settings defined in your php.ini.
+     * - 'cake' - Saves session files in CakePHP's /tmp directory.
+     * - 'database' - Uses CakePHP's database sessions.
+     * - 'cache' - Use the Cache class to save sessions.
+     *
+     * To define a custom session handler, save it at src/Network/Session/<name>.php.
+     * Make sure the class implements PHP's `SessionHandlerInterface` and set
+     * Session.handler to <name>
+     *
+     * To use database sessions, load the SQL file located at config/Schema/sessions.sql
+     */
+    'Session' => [
+        'defaults' => 'php',
+    ],
+];
