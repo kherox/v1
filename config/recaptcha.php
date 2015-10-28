7d799b11b7d6d924249afcc43ccc37d70b3ca708
Gynidark <gynidark@hotmail.com>
2015-10-28 17:47:39 +0100

    Change file scss

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -6,7 +6,7 @@ return [
         'sitekey' => '6LdEgg4TAAAAAJ6Mdo3X49Bp9QT6dO4aYTmN3XVS',
         'secret' => '6LdEgg4TAAAAAPWnWXE6sliAc3QM_5yZHNOz-zmj',
         // https://developers.google.com/recaptcha/docs/language
-        'lang' => 'en',
+        'lang' => 'fr',
         // light or dark
         'theme' => 'light',
         // image or audio
