36a674e480fe9f1bd24cb16f0bf3fceaf293db97
Gynidark <gynidark@hotmail.com>
2015-10-29 11:04:44 +0100

    Delete for variable markdown

--- webroot/js/inc/markdown.js
+++ webroot/js/inc/markdown.js
@@ -5,17 +5,7 @@ var myEditor = new MTE(document.getElementById('markdown-editor'), {
     buttons: {
         rule: false,
         undo: false,
-        redo: false
-    }
-});
-
-var editor = new MTE(document.getElementById('editor'),{
-    tabSize: '    ',
-    shortcut: true,
-
-    buttons: {
-        rule: false,
-        undo: false,
-        redo: false
+        redo: false,
+        ol: false
     }
 });
